3eb1602d4952cbc1f0dee885d149b47573539919
oussama <oussama.mabsout@edu.esiee.fr>
2023-06-12 04:08:54 +0200

    module 2 entier plus debut de module 3 mais api non fonctionnelle

--- FoodDatabaseScreen.js
+++ FoodDatabaseScreen.js
@@ -1,13 +1,106 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
 function FoodDatabaseScreen() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [foodData, setFoodData] = useState(null);
+
+  const handleSearch = () => {
+    const endpoint = 'https://trackapi.nutritionix.com/v2/search/item';
+    const nixItemId = '513fc9e73fe3ffd40300109f';
+    const appId = '73692cb5';
+    const appKey = 'e2ad41d5f6891b1286283dc316696bb8';
+
+    fetch(`${endpoint}?nix_item_id=${nixItemId}`, {
+      headers: {
+        'x-app-id': appId,
+        'x-app-key': appKey,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Network response was not ok.');
+        }
+      })
+      .then((data) => {
+        if (data && data.foods && data.foods.length > 0) {  
+          const foodItem = data.foods[0];
+          console.log(foodItem);
+          setFoodData(foodItem);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-    <View>
-      <Text>Current Page: Food Database Screen</Text>
-      {}
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Enter food name"
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+        />
+        <TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
+          <Text style={styles.searchButtonText}>Search</Text>
+        </TouchableOpacity>
+      </View>
+      {foodData && (
+        <View style={styles.foodDataContainer}>
+          <Text style={styles.foodName}>{foodData.food_name}</Text>
+          <Text>Brand: {foodData.brand_name}</Text>
+          <Text>Calories: {foodData.nf_calories}</Text>
+        </View>
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'top',
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    marginTop: 16,
+  },
+  searchInput: {
+    flex: 1,
+    fontSize: 16,
+  },
+  searchButton: {
+    backgroundColor: 'blue',
+    borderRadius: 8,
+    marginLeft: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  searchButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  foodDataContainer: {
+    marginTop: 16,
+  },
+  foodName: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 8,
+  },
+});
 
 export default FoodDatabaseScreen;
--- HealthGoalsScreen.js
+++ HealthGoalsScreen.js
@@ -1,13 +1,303 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  Modal,
+  Button,
+  StyleSheet,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
 
-function HealthGoalsScreen() {
+const CaloricIntakeResult = ({ adjustedCaloricIntake }) => {
   return (
-    <View>
-      <Text>Current Page: Health Goals Screen</Text>
-      {}
+    <View style={styles.resultContainer}>
+      <Text style={styles.resultText}>Caloric Intake:</Text>
+      <Text style={styles.caloricIntake}>{adjustedCaloricIntake}</Text>
     </View>
+  );
+};
+
+export default function App() {
+  const [age, setAge] = useState('');
+  const [gender, setGender] = useState('');
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [activityLevel, setActivityLevel] = useState('');
+  const [healthGoal, setHealthGoal] = useState('');
+  const [isGenderDropdownVisible, setIsGenderDropdownVisible] = useState(false);
+  const [isActivityLevelDropdownVisible, setIsActivityLevelDropdownVisible] = useState(false);
+  const [isHealthGoalDropdownVisible, setIsHealthGoalDropdownVisible] = useState(false);
+  const [adjustedCaloricIntake, setAdjustedCaloricIntake] = useState('');
+
+  const handleAgeChange = (text) => {
+    setAge(text);
+  };
+
+  const handleGenderChange = (selectedGender) => {
+    setGender(selectedGender);
+    setIsGenderDropdownVisible(false);
+  };
+
+  const handleHeightChange = (text) => {
+    setHeight(text);
+  };
+
+  const handleWeightChange = (text) => {
+    setWeight(text);
+  };
+
+  const handleActivityLevelChange = (selectedActivityLevel) => {
+    setActivityLevel(selectedActivityLevel);
+    setIsActivityLevelDropdownVisible(false);
+  };
+
+  const handleHealthGoalChange = (selectedHealthGoal) => {
+    setHealthGoal(selectedHealthGoal);
+    setIsHealthGoalDropdownVisible(false);
+  };
+
+  const calculateBMR = () => {
+    let bmr = 0;
+
+    if (gender === 'Male') {
+      bmr = 88.362 + 13.397 * weight + 4.799 * height - 5.677 * age;
+    } else if (gender === 'Female') {
+      bmr = 447.593 + 9.247 * weight + 3.098 * height - 4.33 * age;
+    }
+
+    return bmr.toFixed(0);
+  };
+
+  const handleSubmit = () => {
+    const bmr = calculateBMR();
+    const calor = calculateAdjustedCaloricIntake(bmr, healthGoal, activityLevel);
+    setAdjustedCaloricIntake(calor);
+  };
+
+  const isSubmitDisabled = !age || !gender || !height || !weight || !activityLevel || !healthGoal;
+
+  const calculateAdjustedCaloricIntake = (bmr, healthGoal, activityLevel) => {
+    let adjustedCaloricIntake = 0;
+    switch (activityLevel) {
+      case 'Sedentary':
+        adjustedCaloricIntake = bmr * 1.2;
+        break;
+      case 'Light Exercise':
+        adjustedCaloricIntake = bmr * 1.375;
+        break;
+      case 'Moderate Exercise':
+        adjustedCaloricIntake = bmr * 1.55;
+        break;
+      case 'Heavy Exercise':
+        adjustedCaloricIntake = bmr * 1.725;
+        break;
+      case 'Extra Active':
+        adjustedCaloricIntake = bmr * 1.9;
+        break;
+      default:
+        adjustedCaloricIntake = bmr;
+        break;
+    }
+
+    switch (healthGoal) {
+      case 'Weight Loss':
+        adjustedCaloricIntake -= adjustedCaloricIntake * 0.1;
+        break;
+      case 'Weight Gain':
+        adjustedCaloricIntake += adjustedCaloricIntake * 0.1;
+        break;
+    }
+
+    return adjustedCaloricIntake.toFixed(0);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="Age in years"
+          onChangeText={handleAgeChange}
+          value={age}
+          keyboardType="numeric"
+        />
+        <View style={styles.dropdownContainer}>
+          <TouchableOpacity
+            style={styles.dropdownButton}
+            onPress={() => setIsGenderDropdownVisible(true)}>
+            <Text>{gender ? gender : 'Select Gender'}</Text>
+          </TouchableOpacity>
+          {isGenderDropdownVisible && (
+            <Modal animationType="slide" transparent visible={isGenderDropdownVisible}>
+              <View style={styles.dropdownModalContainer}>
+                <TouchableOpacity
+                  style={styles.dropdownItem}
+                  onPress={() => handleGenderChange('Male')}>
+                  <Text>Male</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.dropdownItem}
+                  onPress={() => handleGenderChange('Female')}>
+                  <Text>Female</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.dropdownItem}
+                  onPress={() => handleGenderChange('Other')}>
+                  <Text>Other</Text>
+                </TouchableOpacity>
+                <Button title="Close" onPress={() => setIsGenderDropdownVisible(false)} />
+              </View>
+            </Modal>
+          )}
+        </View>
+        <TextInput
+          style={styles.input}
+          placeholder="Height in cm"
+          onChangeText={handleHeightChange}
+          value={height}
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Weight in kg"
+          onChangeText={handleWeightChange}
+          value={weight}
+          keyboardType="numeric"
+        />
+        <View style={styles.dropdownContainer}>
+          <TouchableOpacity
+            style={styles.dropdownButton}
+            onPress={() => setIsActivityLevelDropdownVisible(true)}>
+            <Text>{activityLevel ? activityLevel : 'Select Activity Level'}</Text>
+          </TouchableOpacity>
+          {isActivityLevelDropdownVisible && (
+            <Modal animationType="slide" transparent visible={isActivityLevelDropdownVisible}>
+              <View style={styles.dropdownModalContainer}>
+                <TouchableOpacity
+                  style={styles.dropdownItem}
+                  onPress={() => handleActivityLevelChange('Sedentary')}>
+                  <Text>Sedentary</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.dropdownItem}
+                  onPress={() => handleActivityLevelChange('Light Exercise')}>
+                  <Text>Light Exercise</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.dropdownItem}
+                  onPress={() => handleActivityLevelChange('Moderate Exercise')}>
+                  <Text>Moderate Exercise</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.dropdownItem}
+                  onPress={() => handleActivityLevelChange('Heavy Exercise')}>
+                  <Text>Heavy Exercise</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.dropdownItem}
+                  onPress={() => handleActivityLevelChange('Extra Active')}>
+                  <Text>Extra Active</Text>
+                </TouchableOpacity>
+                <Button title="Close" onPress={() => setIsActivityLevelDropdownVisible(false)} />
+              </View>
+            </Modal>
+          )}
+        </View>
+        <View style={styles.dropdownContainer}>
+          <TouchableOpacity
+            style={styles.dropdownButton}
+            onPress={() => setIsHealthGoalDropdownVisible(true)}>
+            <Text>{healthGoal ? healthGoal : 'Select Health Goal'}</Text>
+          </TouchableOpacity>
+          {isHealthGoalDropdownVisible && (
+            <Modal animationType="slide" transparent visible={isHealthGoalDropdownVisible}>
+              <View style={styles.dropdownModalContainer}>
+                <TouchableOpacity
+                  style={styles.dropdownItem}
+                  onPress={() => handleHealthGoalChange('Weight Loss')}>
+                  <Text>Weight Loss</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.dropdownItem}
+                  onPress={() => handleHealthGoalChange('Weight Maintenance')}>
+                  <Text>Weight Maintenance</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.dropdownItem}
+                  onPress={() => handleHealthGoalChange('Weight Gain')}>
+                  <Text>Weight Gain</Text>
+                </TouchableOpacity>
+                <Button title="Close" onPress={() => setIsHealthGoalDropdownVisible(false)} />
+              </View>
+            </Modal>
+          )}
+        </View>
+        <Button title="Submit" onPress={handleSubmit} disabled={isSubmitDisabled} />
+        {adjustedCaloricIntake !== '' && (
+          <CaloricIntakeResult adjustedCaloricIntake={adjustedCaloricIntake} />
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
-export default HealthGoalsScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  dropdownContainer: {
+    width: '100%',
+    marginBottom: 10,
+  },
+  dropdownButton: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dropdownModalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgb(255,255,255)',
+  },
+  dropdownItem: {
+    width: '100%',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  resultContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  resultText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  caloricIntake: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+});
