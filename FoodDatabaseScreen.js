c585573acaed0796bd59f06cb17ecb7a5dceef54
oussama <oussama.mabsout@edu.esiee.fr>
2023-06-13 10:17:45 +0200

    module 2  meal plan pas definitif

--- FoodDatabaseScreen.js
+++ FoodDatabaseScreen.js
@@ -1,17 +1,40 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import React, { useState, useRef } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Modal,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView,
+} from 'react-native';
 
 function FoodDatabaseScreen() {
   const [searchQuery, setSearchQuery] = useState('');
-  const [foodData, setFoodData] = useState(null);
+  const [foodDataList, setFoodDataList] = useState([]);
+  const [selectedFoodData, setSelectedFoodData] = useState(null);
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedMeal, setSelectedMeal] = useState('Breakfast');
+  const [quantity, setQuantity] = useState('');
+  const quantityInputRef = useRef(null);
+  const [mealPlan, setMealPlan] = useState({
+    Breakfast: [],
+    Lunch: [],
+    Snack: [],
+    Dinner: [],
+  });
 
   const handleSearch = () => {
-    const endpoint = 'https://trackapi.nutritionix.com/v2/search/item';
-    const nixItemId = '513fc9e73fe3ffd40300109f';
+    const endpoint = 'https://trackapi.nutritionix.com/v2/search/instant';
     const appId = '73692cb5';
-    const appKey = 'e2ad41d5f6891b1286283dc316696bb8';
+    const appKey = '675436548ed44b83cf6af4a1bfe51486';
 
-    fetch(`${endpoint}?nix_item_id=${nixItemId}`, {
+    fetch(`${endpoint}?query=${searchQuery}`, {
       headers: {
         'x-app-id': appId,
         'x-app-key': appKey,
@@ -25,38 +48,146 @@ function FoodDatabaseScreen() {
         }
       })
       .then((data) => {
-        if (data && data.foods && data.foods.length > 0) {  
-          const foodItem = data.foods[0];
-          console.log(foodItem);
-          setFoodData(foodItem);
+        if (data && data.branded && data.branded.length > 0) {
+          setFoodDataList(data.branded);
+        } else if (data && data.common && data.common.length > 0) {
+          setFoodDataList(setFoodDataList.concat(data.common));
+          console.log(data.common);
+        } else if (data && data.self && data.self.length > 0) {
+          setFoodDataList(setFoodDataList.concat(data.self));
+          console.log(data.self);
+        } else {
+          setFoodDataList([]);
+          setError('Food not found in the database.');
         }
       })
       .catch((error) => {
         console.error(error);
+        setFoodDataList([]);
+        setError('An error occurred. Please try again later.');
       });
   };
 
+  const handleAddToMealPlan = () => {
+    if (selectedFoodData) {
+      const updatedMealPlan = { ...mealPlan };
+      updatedMealPlan[selectedMeal].push({
+        food: selectedFoodData,
+        quantity: parseFloat(quantity),
+      });
+      setMealPlan(updatedMealPlan);
+      setSelectedMeal('Breakfast');
+      setQuantity('');
+      setSelectedFoodData(null);
+      setShowModal(false);
+    }
+  };
+
+  const handleDismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.searchContainer}>
-        <TextInput
-          style={styles.searchInput}
-          placeholder="Enter food name"
-          value={searchQuery}
-          onChangeText={setSearchQuery}
-        />
-        <TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
-          <Text style={styles.searchButtonText}>Search</Text>
-        </TouchableOpacity>
-      </View>
-      {foodData && (
-        <View style={styles.foodDataContainer}>
-          <Text style={styles.foodName}>{foodData.food_name}</Text>
-          <Text>Brand: {foodData.brand_name}</Text>
-          <Text>Calories: {foodData.nf_calories}</Text>
+    <TouchableWithoutFeedback onPress={handleDismissKeyboard}>
+      <View style={styles.container}>
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Enter food name"
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+          />
+          <TouchableOpacity style={styles.searchButton} onPress={handleSearch}>
+            <Text style={styles.searchButtonText}>Search</Text>
+          </TouchableOpacity>
         </View>
-      )}
-    </View>
+        {error && <Text style={styles.errorText}>{error}</Text>}
+        {foodDataList.length > 0 && (
+          <ScrollView style={styles.scrollView}>
+            <View style={styles.foodDataContainer}>
+              <Text style={styles.resultsText}>Search Results:</Text>
+              {foodDataList.map((foodData) => (
+                <TouchableOpacity
+                  key={foodData.food_name}
+                  style={styles.foodItem}
+                  onPress={() => setSelectedFoodData(foodData)}>
+                  <Text style={styles.foodName}>{foodData.food_name}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </ScrollView>
+        )}
+        {selectedFoodData != null && (
+          <View style={styles.selectedFoodContainer}>
+            <Text style={styles.selectedFoodText}>Selected Food:</Text>
+            <Text style={styles.selectedFoodName}>{selectedFoodData.food_name}</Text>
+            <Text style={styles.selectedFoodCalories}>
+              Calories: {Math.round(selectedFoodData.nf_calories)}
+            </Text>
+            <Text style={styles.selectedFoodQuantity}>
+              Serving quantity: {selectedFoodData.serving_qty} {selectedFoodData.serving_unit}
+            </Text>
+            {selectedFoodData.photo && selectedFoodData.photo.thumb && (
+              <Image
+                source={{ uri: selectedFoodData.photo.thumb }}
+                style={styles.selectedFoodImage}
+              />
+            )}
+          </View>
+        )}
+
+        <TouchableOpacity
+          style={styles.addToMealButton}
+          onPress={() => setShowModal(true)}
+          disabled={!selectedFoodData}>
+          <Text style={styles.addToMealButtonText}>Add to Meal Plan</Text>
+        </TouchableOpacity>
+
+        <Modal visible={showModal} animationType="slide">
+          <TouchableWithoutFeedback onPress={handleDismissKeyboard}>
+            <View style={styles.modalContainer}>
+              <Text style={styles.modalTitle}>Add to Meal Plan</Text>
+              <View style={styles.modalContent}>
+                <Text style={styles.modalLabel}>Serving:</Text>
+
+                <TextInput
+                  ref={quantityInputRef}
+                  style={styles.modalInput}
+                  placeholder="Enter quantity"
+                  placeholderTextColor="#ccc"
+                  value={quantity}
+                  onChangeText={setQuantity}
+                  keyboardType="numeric"
+                />
+                {selectedFoodData != null && (
+                  <Text style={styles.modalQuantity}>
+                    Serving = {selectedFoodData.serving_qty * quantity}{' '}
+                    {selectedFoodData.serving_unit}
+                  </Text>
+                )}
+
+                <Text style={styles.modalLabel}>Meal:</Text>
+                <Picker
+                  selectedValue={selectedMeal}
+                  style={styles.picker}
+                  onValueChange={(itemValue) => setSelectedMeal(itemValue)}>
+                  <Picker.Item label="Breakfast" value="Breakfast" />
+                  <Picker.Item label="Lunch" value="Lunch" />
+                  <Picker.Item label="Dinner" value="Dinner" />
+                  <Picker.Item label="Snack" value="Snack" />
+                </Picker>
+                <TouchableOpacity style={styles.modalButton} onPress={handleAddToMealPlan}>
+                  <Text style={styles.modalButtonText}>Add</Text>
+                </TouchableOpacity>
+              </View>
+              <TouchableOpacity style={styles.closeModalButton} onPress={() => setShowModal(false)}>
+                <Text style={styles.closeModalButtonText}>Close</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableWithoutFeedback>
+        </Modal>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -65,7 +196,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     alignItems: 'center',
-    justifyContent: 'top',
+    justifyContent: 'center',
   },
   searchContainer: {
     flexDirection: 'row',
@@ -74,7 +205,7 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     paddingHorizontal: 12,
     paddingVertical: 8,
-    marginTop: 16,
+    marginBottom: 16,
   },
   searchInput: {
     flex: 1,
@@ -93,13 +224,136 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 16,
   },
+  scrollView: {
+    flex: 1,
+  },
   foodDataContainer: {
-    marginTop: 16,
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  resultsText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  foodItem: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    marginBottom: 8,
+    alignItems: 'center',
   },
   foodName: {
+    fontSize: 16,
+  },
+  selectedFoodContainer: {
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  selectedFoodText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  selectedFoodName: {
     fontWeight: 'bold',
     fontSize: 18,
     marginBottom: 8,
+  },
+  selectedFoodCalories: {
+    fontSize: 16,
+    marginBottom: 8,
+    color: 'gray',
+  },
+  selectedFoodQuantity: {
+    fontSize: 14,
+    color: 'gray',
+  },
+  selectedFoodImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+    marginBottom: 8,
+  },
+  addToMealButton: {
+    backgroundColor: 'blue',
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 16,
+  },
+  addToMealButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  modalContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  modalContent: {
+    width: '100%',
+  },
+  modalLabel: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  modalQuantity: {
+    fontSize: 16,
+    marginBottom: 8,
+    color: 'gray',
+  },
+  modalInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    marginBottom: 16,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    fontSize: 16,
+  },
+  picker: {
+    marginBottom: 16,
+  },
+  modalButton: {
+    backgroundColor: 'blue',
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 16,
+  },
+  modalButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  closeModalButton: {
+    backgroundColor: 'red',
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  closeModalButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 16,
+    marginBottom: 16,
   },
 });
 
